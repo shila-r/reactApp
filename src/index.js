b7cab8ac92f10afc9bc2df2137eb8cc28b024b93
shila-r <101824675+shila-r@users.noreply.github.com>
2023-08-22 11:20:57 +0430

    updated314444

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+//ReactDOM.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  //document.getElementById("root")
+//);
 //test...
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
